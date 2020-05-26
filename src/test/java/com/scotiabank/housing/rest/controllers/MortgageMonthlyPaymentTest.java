2e20d03e6ed716277e7fcf0b5f198a853af44dcd
Diego Bayona Gomez <diegobayonag@gmail.com>
2020-05-26 00:38:41 -0500

    JUnitTest

--- src/test/java/com/scotiabank/housing/rest/controllers/MortgageMonthlyPaymentTest.java
+++ src/test/java/com/scotiabank/housing/rest/controllers/MortgageMonthlyPaymentTest.java
@@ -1,48 +1,50 @@
 package com.scotiabank.housing.rest.controllers;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.scotiabank.housing.rest.models.MortgageInformation;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class MortgageMonthlyPaymentTest {
 
-	private MockMvc mvcMock;
-	
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
 	@Test
-	public void givenCorrectMortgageInformation_thenStatus200()
-	  throws Exception {
-	 
-	    MortgageInformation info = new MortgageInformation();
-	    info.setAnualInterestRateAsPercentage(3.6);
-	    info.setDownPaymentOnTheMortgageLoan(33000);
-	    info.setPropertyPurchasePrice(660000);
-	    info.setLengthOfLoanInYears(30);
-	    
-	 
-	    mvcMock.perform(MockMvcRequestBuilders.post("/v1/mortgageMontlyPayment/calculate")
-	      .contentType(MediaType.APPLICATION_JSON))
-//	      .content("{\"downPaymentOnTheMortgageLoan\" : 10000,"
-//	    		  	+"\"propertyPurchasePrice\" : 360000,"
-//	    		  	+"\"anualInterestRateAsPercentage\" : 200,"
-//	    		  	+"\"lengthOfLoanInYears\" : 15"
-//	    		  	+"}")
-	      .andExpect(MockMvcResultMatchers.status().isOk())
-	      .andExpect(MockMvcResultMatchers.content()
-	    		  .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-	      //.andExpect(jsonPath("$[0].totalMontlyMortgagePayment", is("bob")))
-	      
-	      //
-	      ;
-	}
+	public void givenCorrectMortgageInformation_thenStatus200() throws Exception {
+
+		MortgageInformation info = new MortgageInformation();
+		info.setAnualInterestRateAsPercentage(3.6);
+		info.setDownPaymentOnTheMortgageLoan(33000);
+		info.setPropertyPurchasePrice(660000);
+		info.setLengthOfLoanInYears(30);
+
+		String route = "/v1/mortgageMontlyPayment/calculate";
+		
+		
+		mockMvc.perform(post(route)
+	            .contentType("application/json")
+	            .param("sendWelcomeMail", "true")
+	            .content(objectMapper.writeValueAsString(info)))
+	            .andExpect(status().isOk())
+	            .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+		
+		}
 }
